ac67a420ff27311588ca0836e0a7f0533e6a9719
Niek Kruse <niek@blendle.com>
2017-07-03 13:45:28 +0200

    simplify electron-build

--- scripts/electron-build.js
+++ scripts/electron-build.js
@@ -3,33 +3,44 @@ process.env.NODE_ENV = 'production';
 
 const fs = require('fs-extra');
 const path = require('path');
-var webpack = require('webpack');
-var config = require('../config/webpack.config.electron');
+const webpack = require('webpack');
+const config = require('../config/webpack.config.electron');
 const paths = require('../config/paths');
 const packager = require('electron-packager');
 
 // Copy electronMain and package.json to the build directory
 fs.copySync(paths.appPackageJson, path.join(paths.appBuild, 'package.json'), {
-  dereference: true
+  dereference: true,
 });
+// prettier-ignore
 fs.copySync(
   path.join(paths.serverDir, 'callback.html'),
-  path.join(paths.appBuild, 'callback.html'), {
-    dereference: true
-  });
-
-webpack(config).run((err, stats) => {
-  if (err) {
-    console.log('Failed to compose', [err]);
-    process.exit(1);
+  path.join(paths.appBuild, 'callback.html'),
+  {
+    dereference: true,
   }
+);
+// prettier-ignore
+fs.copySync(
+  paths.appElectronMain,
+  path.join(paths.appBuild, 'src/electronMain.js')
+);
+// prettier-ignore
+fs.copySync(
+  path.join(paths.appSrc, 'electron'),
+  path.join(paths.appBuild, 'src/electron')
+);
+fs.copySync(paths.serverDir, path.join(paths.appBuild, 'server'));
+fs.copySync(paths.appNodeModules, path.join(paths.appBuild, 'node_modules'));
 
-  packager({
+// prettier-ignore
+packager(
+  {
     dir: paths.appBuild,
     asar: false,
     overwrite: true,
-  }, (error, appPaths) => {
+  },
+  (error, appPaths) => {
     console.log('omg!!!', appPaths);
-  });
-});
-
+  }
+);
